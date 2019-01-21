1f3661821f7955f844c707583381a91c8dbc5df0
splo2t <yu990410@naver.com>
2019-01-21 13:23:23 +0900

    'add testGS25'

--- app.py
+++ app.py
@@ -174,8 +174,8 @@ def update_data():
     driver.get(sUrl)
     prevEndCount = "prev"
     global returnData
+    returnData = ""
     while True:
-        time.sleep(3)
         WebDriverWait(driver, 100).until(EC.invisibility_of_element_located((By.CLASS_NAME, "blockUI blockOverlay")))
         #driver.find_element_by_xpath('//*[ @ id = "TOTAL"]').click()
         el = driver.find_element_by_link_text("다음 페이지로 이동")
