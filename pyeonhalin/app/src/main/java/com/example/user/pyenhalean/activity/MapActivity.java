c8788db4a919d78f04e0f746b59ec00c56abcd81
Jang Wonbeom <jtiger958@gmail.com>
2019-01-30 04:35:17 +0900

    update naver map without client id

--- pyeonhalin/app/src/main/java/com/example/user/pyenhalean/activity/MapActivity.java
+++ pyeonhalin/app/src/main/java/com/example/user/pyenhalean/activity/MapActivity.java
@@ -1,22 +1,52 @@
 package com.example.user.pyenhalean.activity;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
 import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
 import com.example.user.pyenhalean.R;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapSdk;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
 
 
-public class MapActivity extends BaseActivity {
-
+public class MapActivity extends BaseActivity implements OnMapReadyCallback {
+    NaverMap naverMap = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         addToolbar();
 
+        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+
+        mapFragment.getMapAsync(this);
+
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         configBottomNavigation(MapActivity.this,navigation);
+    }
+
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.505122, 126.957090));
+        marker.setMap(this.naverMap);
+
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(37.505122, 126.957090));
+        naverMap.moveCamera(cameraUpdate);
     }
 
 }
